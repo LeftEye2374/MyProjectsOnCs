03dfb2ff7947dab93d561c0b54e64be5443029ba
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-02 18:50:34 +0300

    create SqlDbContext

---
+++ NetWatch/SqlDbContext/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NetWatch.SqlDbContext
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<SqlDbContext>
+    {
+        public SqlDbContext CreateDbContext(string[] args)
+        {
+            var optionalBuilder = new DbContextOptionsBuilder<SqlDbContext>();
+            var connectionString = @"server=.,5433;Database=AutoLotSamples;User Id=sa;Password=P@ssw0rd; TrustServerCertificate=true";
+            optionalBuilder.UseSqlServer(connectionString);
+            return new SqlDbContext(optionalBuilder.Options);
+        }
+    }
+}
---
+++ NetWatch/SqlDbContext/SqlDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetWatch.SqlDbContext
+{
+    public partial class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
