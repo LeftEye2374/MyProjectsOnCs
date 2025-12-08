e94acecd09ba35c155c784164909d457c53ff424
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-08 23:29:41 +0300

    create models

---
+++ SimpleMobileApp/HabitTracker/Models/Habit.cs
@@ -0,0 +1,15 @@
+﻿namespace HabitTracker.Models
+{
+    public class Habit
+    {
+        public int Id { get; set; }
+        public strign Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Color { get; set; }
+        public bool isActive { get; set; }
+        public bool isActive { get; set; }
+
+        public virtual ICollection<HabitLog> HabitLogs { get; set; } = new List<HabitLog>();
+    }
+}
---
+++ SimpleMobileApp/HabitTracker/Models/HabitLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitTracker.Models
+{
+    public class HabitLog
+    {
+        public int Id { get; set; }
+        public int HabitId { get; set; }
+        public virtual Habit Habit { get; set; } = null!;
+        public DateTime CompletedDate { get; set; }
+        public string Notes { get; set; } = string.Empty;
+    }
+}
