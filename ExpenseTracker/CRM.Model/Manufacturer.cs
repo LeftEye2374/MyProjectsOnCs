57f1293e9308f23adf476d8ca55ea4d8b703e60e
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-09 13:03:49 +0300

    create models

--- ExpenseTracker/CRM.Model/Manufacturer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CRM.Model
-{
-    public class Manufacturer : BaseEntity
-    {
-        public required string Name { get; set; }
-        public required ContactInfo ContactInfo { get; set; }
-        public ICollection<Product> Products { get; set; } = new List<Product>();
-    }
-}
