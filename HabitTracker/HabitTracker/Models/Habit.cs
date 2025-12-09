3497fedabb4339033f931dc2971976a4c85689c5
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-09 13:17:23 +0300

    ...

---
+++ HabitTracker/HabitTracker/Models/Habit.cs
@@ -0,0 +1,14 @@
+﻿namespace HabitTracker.Models
+{
+    public class Habit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string Color { get; set; } = "#007AFF";
+        public bool IsActive { get; set; } = true;
+
+        public virtual ICollection<HabitLog> HabitLogs { get; set; } = new List<HabitLog>();
+    }
+}
---
+++ HabitTracker/HabitTracker/Models/HabitLog.cs
@@ -0,0 +1,12 @@
+﻿namespace HabitTracker.Models
+{
+    public class HabitLog
+    {
+        public int Id { get; set; }
+        public int HabitId { get; set; }
+        public virtual Habit Habit { get; set; } = null!;
+        public DateTime CompletedDate { get; set; }
+        public string Notes { get; set; } = string.Empty;
+    }
+}
+
