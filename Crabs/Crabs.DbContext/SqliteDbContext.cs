e8f0b107428ae90c83f386bee05d8e6ed23a3a26
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-15 01:04:33 +0300

    crabiki

---
+++ Crabs/Crabs.DbContext/SqliteDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Crabs.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Crabs.SqliteDbContext
+{
+    public class SqliteDbContext : DbContext
+    {
+        private readonly string _dbPath;
+        public DbSet<Autorization> Items { get; set; }
+
+        public SqliteDbContext(DbContextOptions<SqliteDbContext> options) : base(options)
+        {
+        }
+
+        protected SqliteDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var dbPath = _dbPath;
+                optionsBuilder.UseSqlite($"Data Source={dbPath}");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Autorization>(entity =>
+            {
+                entity.ToTable("Autorizations");
+                entity.Property(entity => entity.Username);
+                entity.Property(entity => entity.Password);
+            });
+        }
+    }
+}
---
+++ Crabs/Crabs.Models/Autorization.cs
@@ -0,0 +1,9 @@
+﻿namespace Crabs.Models
+{
+    public class Autorization
+    {
+        public Guid Id { get; set; }         
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
