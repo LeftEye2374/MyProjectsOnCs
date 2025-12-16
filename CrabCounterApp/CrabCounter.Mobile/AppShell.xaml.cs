5d19e2b2bc93dde2d602bbb019d1d0c4658a366e
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-16 22:29:34 +0300

    start mobile

---
+++ CrabCounterApp/CrabCounter.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CrabCounter.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CrabCounterApp/CrabCounter.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CrabCounter.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
