fe7874af9ece0f45ff017d6bcc64b52e2cf4e5f8
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-16 22:28:35 +0300

    SqliteDbContext created

---
+++ CrabCounterApp/SqliteDbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CrabCounter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SqliteDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<CrabCounter> crabs { get; set; }
+
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=crabiki.db");
+        }
+    }
+}
