33df07262bb4d54697a4f97223c714dd6f54931e
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-16 21:34:19 +0300

    create models

---
+++ CrabCounterApp/CrabCounter.Models/CrabCounter.cs
@@ -0,0 +1,9 @@
+﻿namespace CrabCounter.Models
+{
+    public class CrabCounter
+    {
+        public Guid Id { get; set; }
+        public int Count { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
