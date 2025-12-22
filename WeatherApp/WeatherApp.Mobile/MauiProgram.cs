34a4ba4aa7c3218a50bd39c4f574b28ec44767ed
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-22 18:07:01 +0300

    MauiProgram.cs was seted

--- WeatherApp/WeatherApp.Mobile/MauiProgram.cs
+++ WeatherApp/WeatherApp.Mobile/MauiProgram.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+using AppDbContext;
+using Microsoft.EntityFrameworkCore;
+using WeatherApp.Mobile.Services;
+using WeatherApp.Mobile.ViewModel;
 
 namespace WeatherApp.Mobile
 {
@@ -7,19 +12,34 @@ namespace WeatherApp.Mobile
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkit();
 
+            string DbPath = Path.Combine(FileSystem.AppDataDirectory, "WeatherApp.db");
+            builder.Services.AddDbContext<SqliteDbContexrt>(options => options.UseSqlite($"Data Source = {DbPath}"));
+
+            builder.Services.AddSingleton<HttpClient>();
+            builder.Services.AddSingleton<WeatherService>();
+
+            builder.Services.AddTransient<MainPageViewModel>();
+
+            builder.Services.AddSingleton<MainPage>();
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
+            var app = builder.Build();
 
-            return builder.Build();
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<SqliteDbContexrt>();
+                db.Database.EnsureCreated();
+            }
+
+
+                return app;
         }
     }
 }
