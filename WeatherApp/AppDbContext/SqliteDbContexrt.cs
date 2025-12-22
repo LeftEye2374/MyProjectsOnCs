dfacdb33e9ba01a7457e6af101a2348d88109e9e
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-22 13:21:00 +0300

    dbcontext

---
+++ WeatherApp/AppDbContext/SqliteDbContexrt.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherApp.Models.EntityModels;
+
+namespace AppDbContext
+{
+    public class SqliteDbContexrt : DbContext
+    {
+        public DbSet<FavoriteCity> FavoriteCities { get; set; }
+        public DbSet<WeatherCache> WeatherCaches { get; set; }
+
+        public SqliteDbContexrt(DbContextOptions<SqliteDbContexrt> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FavoriteCity>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.HasIndex(e => e.Name).IsUnique();
+            });
+
+            modelBuilder.Entity<WeatherCache>(entity =>
+            {
+                entity.HasKey(e => e.CityId);
+                entity.Property(e => e.JsonData).IsRequired();
+                entity.Property(e => e.CachedAt).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- WeatherApp/WeatherApp.Models/EntityModels/FavoriteCity.cs
+++ WeatherApp/WeatherApp.Models/EntityModels/FavoriteCity.cs
@@ -2,7 +2,6 @@
 {
     public class FavoriteCity
     {
-        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
         public double Latitude { get; set; }
