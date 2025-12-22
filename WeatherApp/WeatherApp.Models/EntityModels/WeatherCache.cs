c29f11ada5778140240fb0ae96dd452be04ef126
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-22 13:11:31 +0300

    Entity models created

---
+++ WeatherApp/WeatherApp.Models/EntityModels/WeatherCache.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherApp.Models.EntityModels
+{
+    public class WeatherCache
+    {
+        public string CityId { get; set; }  // Используйте CityId из API
+        public string JsonData { get; set; }
+        public DateTime CachedAt { get; set; }
+    }
+}
