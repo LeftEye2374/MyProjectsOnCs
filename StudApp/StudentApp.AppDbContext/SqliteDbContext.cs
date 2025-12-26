83d61835bd5e92edab94aa29e5f9374283a6d368
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-26 18:44:53 +0300

    app db context

---
+++ StudApp/StudentApp.AppDbContext/SqliteDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudApp.Models;
+
+namespace StudentApp.AppDbContext
+{
+    public class SqliteDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public SqliteDbContext(DbContextOptions<SqliteDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>();
+            modelBuilder.Entity<Student>();
+        }
+    }
+}
