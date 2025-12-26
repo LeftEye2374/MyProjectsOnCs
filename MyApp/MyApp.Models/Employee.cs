356e2b6135a426d07eeed4705a0d62c4d26bb7a0
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-26 16:31:40 +0300

    create models

---
+++ MyApp/MyApp.Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace StudApp.Models
+{
+    public class Employee : BaseModel
+    {
+        public string Position { get; set; }
+        public DateTime StartWork { get; set; }
+        public DateTime EndWork { get; set; }
+        public string Shift { get; set; }
+    }
+}
---
+++ MyApp/MyApp.Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace StudApp.Models
+{
+    public class Student : BaseModel
+    {
+        public DateTime Date { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
