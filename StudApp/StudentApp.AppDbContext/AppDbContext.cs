d54170d1733ec0b6c0f323b198900bf887876448
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-26 17:05:52 +0300

    .

---
+++ StudApp/StudentApp.AppDbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudApp.Models;
+
+namespace StudentApp.AppDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ StudApp/StudentApp.AppDbContext/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace StudentApp.AppDbContext
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionalBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = "";
+            optionalBuilder.UseSqlServer(connectionString);
+            return new AppDbContext(optionalBuilder.Options);
+        }
+    }
+}
