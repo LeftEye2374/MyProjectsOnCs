7962d687664654a8eb686854d8476304c47c42ea
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-12-26 18:53:49 +0300

    start mobile version

---
+++ StudApp/StudApp.Mobile/View/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using StudApp.Mobile.ViewModel;
+
+namespace StudApp.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(MainPageViewModel mainPage)
+        {
+            InitializeComponent();
+            BindingContext = mainPage;
+        }
+    }
+}
