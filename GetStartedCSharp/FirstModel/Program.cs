724c180e496b06d4fb44b5b9eb590bf7aa5d5fa4
Borissov M.R. <borissov.mikhail2374@mail.ru>
2026-01-26 21:49:48 +0300

    ...

---
+++ GetStartedCSharp/FirstModel/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstModel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int x = 15;
+            int y = 34;
+            if(x > y)
+            {
+                Console.WriteLine("x > y");
+            }
+            
+        }
+    }
+}
